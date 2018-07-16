e78905d3a93ff2a5d11318f4b2e3784857d0a783
Handerson Marinho <handersonmari@gmail.com>
2018-07-16 10:40:21 -0300

    Minor refactor

--- app/controller/productController.js
+++ app/controller/productController.js
@@ -1,43 +1,32 @@
-var service = require('../service/productService');
+let service = require('../service/productService');
 
-var data = [{
-  name: 'product 1',
-  id: '1'
-}, {
-  name: 'product 2',
-  id: '2'
-}, {
-  name: 'product 3',
-  id: '3'
-}];
+let productController = {
 
-var productController = {
-
-  getAll: function (req, res) {    
+  getAll: (req, res) => {
     let list = service.getAll();
     res.json(list);
   },
 
-  getOne: function (req, res) {
-    var id = req.params.id;    
+  getOne: (req, res) => {
+    var id = req.params.id;
     let data = service.getById(id);
     res.json(data);
   },
 
-  create: function (req, res) {
+  create: (req, res) => {
     let product = req.body;
-    product = service.create(product);        
+    product = service.create(product);
     res.json(product);
   },
 
-  update: function (req, res) {
+  update: (req, res) => {
     var product = req.body;
     product.id = req.params.id;
-    product = service.update(product);    
+    product = service.update(product);
     res.json(product);
   },
 
-  delete: function (req, res) {
+  delete: (req, res) => {
     var id = req.params.id;
     service.delete(id);
     res.json(true);
--- app/service/productService.js
+++ app/service/productService.js
@@ -3,9 +3,48 @@ let uuidv1 = require('uuid/v1');
 let lokidb = new loki('example.db');
 let db = lokidb.addCollection('product');
 
+let productService = {
+
+    // Creates a new product
+    create: (product) => {
+        validate(product);
+        product.id = uuidv1();
+        db.insert(product);
+        return product;
+    },
+
+    // Update the product
+    update: (product) => {
+        if (id == '')
+            throw 'Id is mandatory';
+        validate(product);
+        db.findAndUpdate({ 'id': id, })
+    },
+
+    // Delete an existing product
+    delete: (id) => {
+        if (id == '')
+            throw 'Id is mandatory';
+        db.removeWhere({ 'id': id });
+    },
+
+    // Get all products in db
+    getAll: () => {
+        return db.find();
+    },
+
+    // Gets a product given the id
+    getById: (id) => {
+        if (id == '')
+            throw 'Id is mandatory';
+        return db.findOne({ 'id': id });
+    }
+
+}
+
 // Validate the model in order to see if all mandatory fields
 // has been informed.
-function validateModel(product) {
+function validate(product) {
     if (product.name == '')
         throw 'Product is mandatory.';
 
@@ -14,45 +53,6 @@ function validateModel(product) {
 
     if (product.quantity < 0)
         throw 'Product is mandatory.';
-}
-
-let productService = {
-
-    // Creates a new product
-    create: function (product) {
-        validateModel(product);
-        product.id = uuidv1();
-        db.insert(product);
-        return product;
-    },
-
-    // Update the product
-    update: function (product) {
-        if (id == '')
-            throw 'Id is mandatory';
-        validateModel(product);
-        db.findAndUpdate({ 'id' : id, })
-    },
-
-    // Delete an existing product
-    delete: function (id) {
-        if (id == '')
-            throw 'Id is mandatory';
-        db.removeWhere({ 'id': id });
-    },
-
-    // Get all products in db
-    getAll: function () {
-        return db.find();
-    },
-
-    // Gets a product given the id
-    getById: function (id) {
-        if (id == '')
-            throw 'Id is mandatory';
-        return db.findOne({ 'id' : id });
-    }
-
 }
 
 module.exports = productService;
