004feb0af1635a7055c948d0d48feae8f48f3abd
Handerson Marinho <handersonmari@gmail.com>
2018-07-26 17:41:13 -0300

    Refactor on startup pipeline

--- app/startup.js
+++ app/startup.js
@@ -5,48 +5,58 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
+setupViewRender(app);
+setupRoutes(app);
+startHttpServer(app);
 
-// Set the view engine and enale script base path navigation
-app.set('view engine', 'ejs');
-app.use("/scripts", express.static(__dirname + "/node_modules/"));
+// Setup all dependencies to render pages using EJS.
+function setupViewRender(app) {
+  // Set EJS as view engine and define /script as base path to consume components
+  app.set('view engine', 'ejs');
+  app.use("/scripts", express.static(__dirname + "/node_modules/"));
 
-// Render index page using EJS
-app.get('/', function(req, res) {
- res.render('pages/index');
-});
+  // By default render the index page using EJS folder structure /views/pages/
+  app.get('/', function (req, res) {
+    res.render('pages/index');
+  });
+}
 
-// Enable Cross Origin resource sharing.
-app.all('/*', (req, res, next) => {
-  // Set custom headers for CORS
-  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,Authorization');
+// Setup all routes used by the application.
+function setupRoutes(app) {
+  app.all('/*', (req, res, next) => {
+    // Set custom headers for CORS
+    res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-type,Accept,Authorization');
 
-  if (req.method == 'OPTIONS') {
-    res.status(200).end();
-  } else {
-    next();
-  }
-});
+    if (req.method == 'OPTIONS') {
+      res.status(200).end();
+    } else {
+      next();
+    }
+  });
 
-// Auth Middleware - This will check if the token is valid
-// Only the requests that start with /api/v1/* will be checked for the token.
-// Any URL's that do not follow the below pattern should be avoided unless you 
-// are sure that authentication is not needed
-app.all('/api/v1/*', [require('./engine/authMiddleware')]);
+  // Auth Middleware - This will check if the token is valid
+  // Only the requests that start with /api/v1/* will be checked for the token.
+  // Any URL's that do not follow the below pattern should be avoided unless you 
+  // are sure that authentication is not needed.
+  app.all('/api/v1/*', [require('./engine/authMiddleware')]);
 
-// The list of routes for the application. 
-app.use('/', require('./controller'));
+  // The list of routes for the application. 
+  app.use('/', require('./controller'));
 
-// If no route is matched by now, it must be a 404
-app.use((req, res, next) => {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  // If no route is matched by now, it must be a 404
+  app.use((req, res, next) => {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+}
 
-// Start the server
-app.set('port', process.env.PORT || 3000);
-let server = app.listen(app.get('port'), () => {
-  console.log('Server listening on port ' + server.address().port);
-});
+// Start the express server.
+function startHttpServer(app) {  
+  app.set('port', process.env.PORT || 3000);
+  let server = app.listen(app.get('port'), () => {
+    console.log('Server listening on port ' + server.address().port);
+  });
+}
