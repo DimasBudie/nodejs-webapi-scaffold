e9d1e22c78cbfcc41410d0aff86b33b79dc55aee
Handerson Marinho <handersonmari@gmail.com>
2018-07-27 14:17:48 -0300

    Added now comman list

---
+++ now-commands.sh
@@ -0,0 +1,11 @@
+# New publish
+now
+
+# Change alias after published
+now alias node-restapi-xovzzgbqty.now.sh my-new-alias
+
+# List all containers
+now list
+
+# Remove container
+now rm node-restapi-xovzzgbqty.now.sh
