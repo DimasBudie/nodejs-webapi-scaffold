e5543c07741a903e1a57653c98591b7cd8e660cf
Handerson Marinho <handersonmari@gmail.com>
2018-07-30 15:52:03 -0300

    Dashboard ajustments

--- views/assets/demo/demo.js
+++ views/assets/demo/demo.js
@@ -103,31 +103,31 @@ demo = {
       type: 'line',
 
       data: {
-        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
+        labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
         datasets: [{
             borderColor: "#6bd098",
             backgroundColor: "#6bd098",
             pointRadius: 0,
             pointHoverRadius: 0,
             borderWidth: 3,
-            data: [300, 310, 316, 322, 330, 326, 333, 345, 338, 354]
+            data: [23000, 10000, 60000, 43000, 78500, 3500, 6500, 0, 0, 0, 0, 0]
           },
-          {
-            borderColor: "#f17e5d",
-            backgroundColor: "#f17e5d",
-            pointRadius: 0,
-            pointHoverRadius: 0,
-            borderWidth: 3,
-            data: [320, 340, 365, 360, 370, 385, 390, 384, 408, 420]
-          },
-          {
-            borderColor: "#fcc468",
-            backgroundColor: "#fcc468",
-            pointRadius: 0,
-            pointHoverRadius: 0,
-            borderWidth: 3,
-            data: [370, 394, 415, 409, 425, 445, 460, 450, 478, 484]
-          }
+          // {
+          //   borderColor: "#f17e5d",
+          //   backgroundColor: "#f17e5d",
+          //   pointRadius: 0,
+          //   pointHoverRadius: 0,
+          //   borderWidth: 3,
+          //   data: [320, 340, 365, 360, 370, 385, 390, 384, 408, 420, 433, 380]
+          // },
+          // {
+          //   borderColor: "#fcc468",
+          //   backgroundColor: "#fcc468",
+          //   pointRadius: 0,
+          //   pointHoverRadius: 0,
+          //   borderWidth: 3,
+          //   data: [370, 394, 415, 409, 425, 445, 460, 450, 478, 484, 433, 380]
+          // }
         ]
       },
       options: {
